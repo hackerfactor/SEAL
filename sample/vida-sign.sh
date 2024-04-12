ce8e345fe26cb05b52fdc8dc9a1e8bea05c91890
Neal Krawetz <neal@aeliustech.com>
2024-04-12 09:05:25 -0600

    Specification: More discussion notes. Adding sample/ directory!

---
+++ sample/vida-sign.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+# VIDA sign: sign a file.
+# Requires:
+#   "$1" = private key
+#   "$2" = unsigned input file
+#   "$3" = signed output file
+#   exiftool for adding XMP record
+#   dd for digest
+#   grep for digest
+#   openssl for signing
+#   tr for formatting the signature
+#   sed for inserting the signature
+#   cp for making a backup of the unsigned original
+
+#######################################
+# Check if commands exist
+if ! [ -x "$(command -v exiftool)" ] ; then
+  echo "MISSING: exiftool"
+  exit 1
+fi
+
+if ! [ -x "$(command -v openssl)" ] ; then
+  echo "MISSING: openssl"
+  exit 1
+fi
+
+if ! [ -x "$(command -v grep)" ] ; then
+  echo "MISSING: grep"
+  exit 1
+fi
+
+if ! [ -x "$(command -v dd)" ] ; then
+  echo "MISSING: dd"
+  exit 1
+fi
+
+if ! [ -x "$(command -v tr)" ] ; then
+  echo "MISSING: tr"
+  exit 1
+fi
+
+if ! [ -x "$(command -v sed)" ] ; then
+  echo "MISSING: sed"
+  exit 1
+fi
+
+if ! [ -x "$(command -v cp)" ] ; then
+  echo "MISSING: cp"
+  exit 1
+fi
+
+VERBOSE=0
+if [ "$1" == "-v" ] ; then
+  VERBOSE=1
+  shift
+fi
+
+if [ "$4" == "" ] ; then
+  echo "Usage: $0 [-v] domain private.key unsigned_image.jpg signed_image.jpg"
+  exit 1
+fi
+DOMAIN="$1"
+PRIVATEKEY="$2"
+INFILE="$3"
+OUTFILE="$4"
+
+# signature placeholder; same length as the actual signature.
+PLACEHOLDER=$(echo -n "3031323334353637383930313233343536373839303132333435363738393031323334353637383930313233343536373839303132333435363738393031323334353637383930313233343536373839303132333435363738393031323334353637383930313233343536373839303132333435363738393031323334353637" | openssl base64 | tr -d '\n')
+
+if [ "$VERBOSE" == "1" ] ; then
+  echo "DOMAIN: $DOMAIN"
+  echo "PRIVATEKEY: $PRIVATEKEY"
+  echo "INFILE: $INFILE"
+  echo "OUTFILE: $OUTFILE"
+  echo "PLACEHOLDER: $PLACEHOLDER"
+fi
+
+#######################################
+# Add xmp record with a placeholder value
+
+# Make a backup
+if [ "$VERBOSE" == "1" ] ; then echo "Creating: $OUTFILE" ; fi
+cp "$INFILE" "$OUTFILE"
+
+# Create the file with a placeholder for the signature
+# The place holder is "0123456789..." for 128 characters encoded as base64
+if [ "$VERBOSE" == "1" ] ; then echo "Adding placeholder VIDA record" ; fi
+exiftool -config exiftool-vida.config  -overwrite_original -VIDA="vida=\"1\" b=\"-s,s-\" d=\"$DOMAIN\" ka=\"rsa\" s=\"$PLACEHOLDER\"" "$OUTFILE" > /dev/null 2>&1
+
+# Identify where the placeholder is located
+if [ "$VERBOSE" == "1" ] ; then echo "Identifying signature location" ; fi
+sigstart=$(grep --byte-offset --only-matching --text "$PLACEHOLDER" "$OUTFILE")
+sigstart=${sigstart%:*}
+siglen=${#PLACEHOLDER}
+((sigend=$sigstart + $siglen))
+if [ "$VERBOSE" == "1" ] ; then echo "Signature in file: $siglen bytes from offset $sigstart to $sigend" ; fi
+
+#######################################
+# Generate the signature
+
+# 1. Sign the file (Everything except the signature placeholder.)
+# 2. Use openssl to sign the data using the private key
+# 3. Convert the signature to hex
+# 4. Replace the signature placeholder with the real signature.
+gotsig=$(
+(
+# TBD: Parse the b=range; for now, assume b=-s,s-
+# Everything before the signature
+dd if="$OUTFILE" bs=1 count="$sigstart" 2>/dev/null
+# Everything after the signature
+dd if="$OUTFILE" bs=1 skip="$sigend" 2>/dev/null
+) | openssl dgst -sha256 -sign "$PRIVATEKEY" | openssl base64 | tr -d '\n'
+)
+
+if [ "$VERBOSE" == "1" ] ; then echo "Generated signature: $gotsig"; fi
+
+#######################################
+# Store the signature
+# Do an inline-replace of the placeholder string with the signature.
+sed -i -e "s@$PLACEHOLDER@$gotsig@" "$OUTFILE"
+
+#######################################
+# Double check that the signature didn't change after being inserted
+if [ "$VERBOSE" == "1" ] ; then
+checksig=$(
+(
+# TBD: Parse the b=range; for now, assume b=-s,s-
+# Everything before the signature
+dd if="$OUTFILE" bs=1 count="$sigstart" 2>/dev/null
+# Everything after the signature
+dd if="$OUTFILE" bs=1 skip="$sigend" 2>/dev/null
+) | openssl dgst -sha256 -sign "$PRIVATEKEY" | openssl base64 | tr -d '\n'
+)
+echo "Verified signature: $checksig"
+if [ "$checksig" == "$gotsig" ] ; then
+  echo "Signature matched!"
+else
+  echo "Signature error!"
+  exit 1
+fi
+
+fi
+
+echo "DONE! $OUTFILE is signed using VIDA!"
+
---
+++ sample/vida-verify.sh
@@ -0,0 +1,172 @@
+#!/bin/bash
+# VIDA verify: check a VIDA signature.
+# TBD:
+#   Add support for parsing the various parameters.
+#   This code currently assumes the default: vida=1, ka=rsa, sf=hex, -b=-s,s-
+#   It also has vulnerabilities in case of unchecked quoted variables.
+#   THIS IS FOR TESTING and PROOF-OF-CONCEPT DEMO ONLY.
+
+# Requires:
+#   "$1" = signed file
+#   The domain to query comes from DNS and the VIDA d= parameter.
+#   dig retrieves the DNS TXT field.
+#   sed for formatting the dig result
+#   exiftool for extracting XMP record
+#   dd for digest
+#   grep for digest
+#   openssl for validating the signature
+#   tr for formatting the signature
+
+#######################################
+# Check if commands exist
+if ! [ -x "$(command -v exiftool)" ] ; then
+  echo "MISSING: exiftool"
+  exit 1
+fi
+
+if ! [ -x "$(command -v dig)" ] ; then
+  echo "MISSING: dig"
+  exit 1
+fi
+
+if ! [ -x "$(command -v sed)" ] ; then
+  echo "MISSING: sed"
+  exit 1
+fi
+
+if ! [ -x "$(command -v openssl)" ] ; then
+  echo "MISSING: openssl"
+  exit 1
+fi
+
+if ! [ -x "$(command -v grep)" ] ; then
+  echo "MISSING: grep"
+  exit 1
+fi
+
+if ! [ -x "$(command -v dd)" ] ; then
+  echo "MISSING: dd"
+  exit 1
+fi
+
+if ! [ -x "$(command -v tr)" ] ; then
+  echo "MISSING: tr"
+  exit 1
+fi
+
+if ! [ -x "$(command -v sed)" ] ; then
+  echo "MISSING: sed"
+  exit 1
+fi
+
+if ! [ -x "$(command -v cp)" ] ; then
+  echo "MISSING: cp"
+  exit 1
+fi
+
+VERBOSE=0
+if [ "$1" == "-v" ] ; then
+  VERBOSE=1
+  shift
+fi
+
+if [ "$1" == "" ] ; then
+  echo "Usage: $0 [-v] signed_image.jpg"
+  exit 1
+fi
+INFILE="$1"
+
+if [ "$VERBOSE" == "1" ] ; then echo "INFILE: $INFILE" ; fi
+
+#######################################
+# Extract the VIDA record.
+#   - Make sure the VIDA record exists
+#   - Retrieve the domain name
+#   - Retrieve the signature
+#   TBD: This uses XMP. Add support for PNG, JPEG APP, and other formats.
+vida=$(exiftool -xmp:vida "$INFILE")
+if [ "$vida" == "" ] ; then
+  echo "No VIDA record found in $INFILE"
+  exit 0
+fi
+
+# Find the domain
+domain=""
+if [[ "$vida" =~ ^.*\ d=[\"\']?([0-9a-zA-Z.-]+) ]] ; then
+  domain=${BASH_REMATCH[1]}
+else
+  echo "ERROR: No domain name found in VIDA record."
+  exit 1
+fi
+if [ "$VERBOSE" == "1" ] ; then echo "Domain from file: $domain" ; fi
+
+# Find the signature
+sig=""
+if [[ "$vida" =~ ^.*\ s=[\"\']?([0-9a-zA-Z+/]+=*) ]] ; then
+  sig=${BASH_REMATCH[1]}
+else
+  echo "ERROR: No signature found in VIDA record."
+  exit 1
+fi
+if [ "$VERBOSE" == "1" ] ; then echo "Signature from file: $sig" ; fi
+
+#######################################
+# Get the public key from the domain
+#   TBD: Make sure the TXT record is the correct TXT record.
+#   TBD: Support the different sf= values.
+
+# Look for any TXT records that are for vida=1 and not revoked
+# DNS might break long lines "abcdef" into "abc" "def". Use sed to recombine them.
+dns=$(dig TXT "$domain" | grep TXT | grep 'vida=1' | grep 'ka=rsa' | grep -v ' r=' | sed -e 's@" "@@g')
+if [ "$dns" == "" ] ; then
+  echo "ERROR: No VIDA public key found at $domain"
+  exit 1
+fi
+
+if [[ "$dns" =~ ^.*\ p=[\"\']?([0-9a-zA-Z+/]+) ]] ; then
+  pubkey=${BASH_REMATCH[1]}
+else
+  echo "ERROR: No VIDA public key found in DNS record."
+  exit 1
+fi
+if [ "$VERBOSE" == "1" ] ; then echo "Public key from DNS: $pubkey" ; fi
+
+#######################################
+# Compute the current digest
+#   TBD: Add support for any b= value. Currently assumes b=-s,s-
+
+# Find out where the signature starts
+sigstart=$(grep --byte-offset --only-matching --text "$sig" "$INFILE")
+sigstart=${sigstart%:*}
+siglen=${#sig}
+((sigend=$sigstart+$siglen))
+if [ "$VERBOSE" == "1" ] ; then echo "Signature in file: $siglen bytes from offset $sigstart to $sigend" ; fi
+
+#######################################
+# Decrypt the signature to get the expected digest
+
+# From the command line, openssl only works on files.
+# Use a temp files!
+
+# Store the known public key from DNS
+PubFile="$INFILE.der"
+echo "$pubkey" | openssl base64 -d > "$PubFile"
+
+# Store the known signature from the file
+SigFile="$INFILE.sig"
+echo "$sig" | openssl base64 -d > "$SigFile"
+
+# See if they match!
+  (
+  # TBD: Parse the b=range; for now, assume b=-s,s-
+  # everything up to the signature
+  dd if="$INFILE" bs=1 count=$sigstart 2>/dev/null
+  # everything after the signature
+  dd if="$INFILE" bs=1 skip=$sigend 2>/dev/null
+  ) | openssl dgst -sha256 -verify "$PubFile" -signature "$SigFile"
+# Openssl will say whether it is validated
+
+# Clean up
+unlink "$PubFile"
+unlink "$SigFile"
+
